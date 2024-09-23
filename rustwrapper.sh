72d4ac1ce68eaa29ede3a15f596376b61ae21bc8
okm <nah>
2024-09-22 21:46:26 -0400

    rcon fix

--- rustwrapper.sh
+++ rustwrapper.sh
@@ -12,7 +12,9 @@ tail -f ./rustserverlog.txt | grep "Server startup complete" &
 # whatever this means
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
 #starting=true
-steam-run ./RustDedicated +server.identity "server1" & # theoretically -batchmode can be ommitted, (not a command or smth in logs??)
+steam-run ./RustDedicated +server.identity "server1" +rcon.web false +rcon.port 28016 +rcon.password RCur2vtPyRSTXh34Qksq4pLUns3CyJ9S2YlfXSiXFolWz8VM1j &
+# theoretically -batchmode can be ommitted, (not a command or smth in logs??)
+
 # rcon wrapped
 
 #while $starting;
