4b7e9296cf401a0d4815335389bcadb4578e67f5
okm <nah>
2024-09-24 04:59:09 -0400

    combine launcher and wrapper

--- launch.sh
+++ launch.sh
@@ -43,3 +43,38 @@ rsync --exclude serverauto.cfg --del -r ./cfg/ ./wrapped/rust/server/server1/cfg
 #export NIXPKGS_ALLOW_UNFREE=1
 #nix-shell --pure ./dependencies.nix --run "./rustwrapper.sh"
 
+
+# wrapper
+
+echo "Launching Wrapper..."
+#screen -S rust_server "steam-run ../rust/runds.sh"
+
+cd ./wrapped/rust/
+# print logs
+tail -f ./rustserverlog.txt &
+tail -f ./rustserverlog.txt | grep "Server startup complete" &
+# start server
+
+# whatever this means
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
+#starting=true
+steam-run ./RustDedicated +server.identity "server1" +server.port 28015 +server.level "Procedural Map" +rcon.web false +rcon.port 28016 +rcon.password RCur2vtPyRSTXh34Qksq4pLUns3CyJ9S2YlfXSiXFolWz8VM1j &
+# theoretically -batchmode can be ommitted, (not a command or smth in logs??)
+
+# rcon wrapped
+
+#while $starting;
+#do
+#tail ./rustserverlog.txt | grep -q "Server startup complete"
+#if [[ $? == 0 ]]; then
+#starting=false
+#fi
+#done
+
+while true;
+do
+read -p "> " input
+rcon -H localhost -p 28016 -P RCur2vtPyRSTXh34Qksq4pLUns3CyJ9S2YlfXSiXFolWz8VM1j -n $input
+done
+
+exit
